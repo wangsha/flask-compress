ad4142e090d34b365cf34c0c99ed5752a187d121
Sha <wangsha@users.noreply.github.com>
2023-10-06 07:23:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
